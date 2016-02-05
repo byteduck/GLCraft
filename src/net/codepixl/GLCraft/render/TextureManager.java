b2401bf7878f88e354220436e4856c377ff69add
Codepixl <aaronsonin@yahoo.com>
2016-02-04 18:08:34 -0700

    Don't mind me... Random commit passing through.

--- src/net/codepixl/GLCraft/render/TextureManager.java
+++ src/net/codepixl/GLCraft/render/TextureManager.java
@@ -1,24 +1,22 @@
 package net.codepixl.GLCraft.render;
 
 import java.util.HashMap;
-
-import net.codepixl.GLCraft.item.Item;
-import net.codepixl.GLCraft.item.ItemStack;
-import net.codepixl.GLCraft.world.tile.Tile;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 public class TextureManager {
-	private static HashMap<String,Texture> textures = new HashMap<String,Texture>();
+	private static HashMap<String,String> textures = new HashMap<String,String>();
+	private static int texCount = 0;
 	public static final String TILES = "textures/tiles/";
 	public static final String ITEMS = "textures/items/";
 	public static final String GUIS = "textures/gui/";
 	public static final String MISC = "textures/misc/";
 	private static String currentBoundTexture = "";
-	public static boolean addTexture(String name, String path){
-		if(!textures.containsKey(name)){
-			textures.put(name, Texture.loadTexture(path));
-			return true;
-		}
-		return false;
+	private static boolean madeAtlas = false;
+	public static void addTexture(String name, String path){
+		textures.put(name,path);
+		texCount++;
 	}
 	public static void initTextures(){
 		addTexture("misc.highlight",TILES+"glass.png");
@@ -27,52 +25,19 @@ public class TextureManager {
 		}
 		addTexture("misc.break_8",MISC+"break_7.png");
 	}
-	public static Texture getTexture(String name) {
-		return textures.get(name);
-	}
-	public static void unbind() {
-		Texture.unbind();
-		currentBoundTexture = "";
-	}
-	public static void bindTile(Tile t){
-		if(!currentBoundTexture.equals("tiles."+t.getTextureName())){
-			getTexture("tiles."+t.getTextureName()).bind();
-			currentBoundTexture = "tiles."+t.getTextureName();
-		}
-	}
-	public static void bindItem(Item i){
-		if(!currentBoundTexture.equals("items."+i.getTextureName())){
-			getTexture("tiles."+i.getTextureName()).bind();
-			currentBoundTexture = "items."+i.getTextureName();
-		}
-	}
-	public static void bindItemStack(ItemStack s){
-		if(!s.isNull()){
-			if(s.isTile()){
-				bindTile(s.getTile());
-			}else{
-				bindItem(s.getItem());
+	public static void generateAtlas(){
+		if(!madeAtlas){
+			madeAtlas = true;
+			final int maxWidth = 10;
+			int total = 0;
+			for (String list : textures.values()) {
+			    total++;
 			}
-		}
-	}
-	public static void bindTexture(String textureName){
-		if(!currentBoundTexture.equals(textureName)){
-			getTexture(textureName).bind();
-			currentBoundTexture = textureName;
-		}
-	}
-	public static void bindTileIcon(Tile t){
-		if(!currentBoundTexture.equals("tiles."+t.getIconName())){
-			getTexture("tiles."+t.getIconName()).bind();
-			currentBoundTexture = "tiles."+t.getIconName();
-		}
-	}
-	public static void bindItemStackIcon(ItemStack s) {
-		if(!s.isNull()){
-			if(s.isTile()){
-				bindTileIcon(s.getTile());
-			}else{
-				bindItem(s.getItem());
+			Iterator<Map.Entry<String, String>> it = textures.entrySet().iterator();
+			int height = (int) Math.ceil((float)total/(float)maxWidth);
+			while(it.hasNext()){
+				Map.Entry<String, String> next = it.next();
+				
 			}
 		}
 	}
