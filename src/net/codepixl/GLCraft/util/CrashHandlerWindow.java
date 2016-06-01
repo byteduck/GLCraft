7b05d303b158e88f1a25c9d692395100d8d19308
Codepixl <aaronsonin@yahoo.com>
2016-05-31 20:55:03 -0600

    Added more info to Crash Handler screen

--- src/net/codepixl/GLCraft/util/CrashHandlerWindow.java
+++ src/net/codepixl/GLCraft/util/CrashHandlerWindow.java
@@ -16,7 +16,16 @@ public class CrashHandlerWindow extends JFrame{
 		getContentPane().add(textArea, BorderLayout.CENTER);
 		
 		String stackTrace = org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace(e);
-		textArea.setText("Well, this is no fun. :(\nGLCraft has crashed in thread: "+t+".\n"+stackTrace);
+		textArea.setText("Well, this is no fun. :(\n\n"+
+				"GLCraft has crashed in thread: "+
+				t+
+				"\n\n"+
+				stackTrace+
+				"\nAdditional diagnostic information:\n"+
+				"Max Memory Available to JVM: "+Runtime.getRuntime().maxMemory()/1000000+"MB\n"+
+				"Total Memory Available to JVM: "+Runtime.getRuntime().totalMemory()/1000000+"MB\n"+
+				"Free Memory Available to JVM: "+Runtime.getRuntime().freeMemory()/1000000+"MB\n"+
+				"Operating System: "+System.getProperty("os.name"));
 		
 		setLocationRelativeTo(null);
 		setVisible(true);
