0061218213fcf4ba0989622937c55709fd2f1433
Codepixl <aaronsonin@yahoo.com>
2016-07-09 21:58:28 -0600

    Added textures made by CodedGoat

--- src/net/codepixl/GLCraft/util/TexturePackManager.java
+++ src/net/codepixl/GLCraft/util/TexturePackManager.java
@@ -5,6 +5,15 @@ import java.awt.Toolkit;
 import java.awt.GridLayout;
 import javax.swing.JScrollPane;
 import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JTextPane;
+import java.awt.Color;
+import javax.swing.UIManager;
+import java.awt.SystemColor;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
 
 public class TexturePackManager extends JFrame{
 	public TexturePackManager() {
@@ -17,6 +26,27 @@ public class TexturePackManager extends JFrame{
 		
 		JPanel panel = new JPanel();
 		getContentPane().add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.NORTH);
+		panel_1.setLayout(new GridLayout(2, 0, 0, 0));
+		
+		JLabel lblSelectedTexturePack = new JLabel("Selected texture pack:");
+		lblSelectedTexturePack.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblSelectedTexturePack);
+		
+		JLabel lblDefaultTextures = new JLabel("Default textures");
+		lblDefaultTextures.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblDefaultTextures);
+		
+		JButton btnOpenTexturePack = new JButton("Open texture pack folder");
+		panel.add(btnOpenTexturePack, BorderLayout.SOUTH);
+		
+		JLabel label = new JLabel("");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setIcon(new ImageIcon(TexturePackManager.class.getResource("/textures/icons/icon32.png")));
+		panel.add(label, BorderLayout.CENTER);
 	}
 
 }
