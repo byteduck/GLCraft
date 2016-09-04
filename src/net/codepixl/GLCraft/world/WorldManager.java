e17254af92bf9e85f5d3954b0969179f3b6c32a8
Codepixl <aaronsonin@yahoo.com>
2016-09-04 17:49:42 -0600

    Fixed entities invisible underwater

--- src/net/codepixl/GLCraft/world/WorldManager.java
+++ src/net/codepixl/GLCraft/world/WorldManager.java
@@ -239,15 +239,18 @@ public class WorldManager {
 			while(i.hasNext()){
 				i.next().render(false);
 			}
+			DebugTimer.pauseTimer("chunk_render");
+			//System.out.println(Raytracer.getScreenCenterRay());
+			DebugTimer.startTimer("entity_render");
+			entityManager.render();
+			DebugTimer.endTimer("entity_render");
+			
+			DebugTimer.startTimer("chunk_render");
 			i = toRender.iterator();
 			while(i.hasNext()){
 				i.next().render(true);
 			}
 			DebugTimer.endTimer("chunk_render");
-			//System.out.println(Raytracer.getScreenCenterRay());
-			DebugTimer.startTimer("entity_render");
-			entityManager.render();
-			DebugTimer.endTimer("entity_render");
 		}
 	}
 	
